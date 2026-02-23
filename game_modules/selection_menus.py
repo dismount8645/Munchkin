0b2c21c023a2fd69655bae48b18a310cf53221f2
dismount8645 <235756303+dismount8645@users.noreply.github.com>
2026-02-23 15:52:01 +0100

    add comments
    comments added to explain code

--- game_modules/selection_menus.py
+++ game_modules/selection_menus.py
@@ -2,6 +2,7 @@
 # SELECTION MENUS
 # ============================
 def choose_gender():
+    """Presents a menu for the user to select the character's gender and returns the selection."""
     options = ["Male", "Female", "Other"]
     print("Choose gender:")
     for i, option in enumerate(options, start=1):
@@ -10,6 +11,7 @@ def choose_gender():
 
 
 def choose_class():
+    """Presents a menu for the user to select the character's class and returns the selection."""
     classes = ["Explorer", "Officer", "Mechanic", "Tycoon"]
     print("Choose class:")
     for i, c in enumerate(classes, start=1):
